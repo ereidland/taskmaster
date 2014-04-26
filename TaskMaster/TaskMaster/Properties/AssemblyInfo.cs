53383f7c6e359d40ab0317398a660cb8eadf58e5
Evan Reidland <er@evanreidland.com>
2014-04-25 22:39:56 -0500

    Adjustments to project settings and structure.

--- TaskMaster/TaskMaster/Properties/AssemblyInfo.cs
+++ TaskMaster/TaskMaster/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("evanreidland")]
+[assembly: AssemblyCopyright("Evan Reidland")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
